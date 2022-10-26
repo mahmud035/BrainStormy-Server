e99c66241c53661da24a7b39dd113d70b4c8ad45
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-26 15:31:07 +0600

    get all courses from a specific category and also get signle course with specific course id

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(cors());
 const Port = process.env.PORT || 5000;
 
 const courseCategory = require('./Data/category.json');
+const courses = require('./Data/courses.json');
 
 app.get('/', (req, res) => {
   res.send('Hello from server');
@@ -13,6 +14,29 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
   res.send(courseCategory);
+});
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if (id == '00') {
+    res.send('courses');
+  } else {
+    const categoryCourses = courses.filter(
+      (course) => course.category_id == id
+    );
+    res.send(categoryCourses);
+  }
+});
+
+app.get('/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/course/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course._id == id);
+  res.send(selectedCourse);
+  console.log(req.params.id);
 });
 
 app.listen(Port, () => {
